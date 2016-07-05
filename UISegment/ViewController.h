38301a6933b1b3fafefaf262719c179ca55ef6da
洛洛 <zhy@miaomiao.local>
2016-07-05 09:00:49 +0800

    Initial Commit

---
+++ UISegment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UISegment
+//
+//  Created by zhy on 16/7/5.
+//  Copyright © 2016年 zhy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UISegment/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UISegment
+//
+//  Created by zhy on 16/7/5.
+//  Copyright © 2016年 zhy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
